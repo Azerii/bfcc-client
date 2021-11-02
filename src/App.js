dc9d63c67997bf62453c35add5373d025297fd05
Pelumi Codes <pel@Pelumis-MacBook-Pro.local>
2021-11-02 16:54:21 +0100

    lazy-loading

--- src/App.js
+++ src/App.js
@@ -1,8 +1,3 @@
-import Contact_Us from "pages/Contact_Us";
-import Landing from "pages/Landing";
-import PrivacyPolicy from "pages/PrivacyPolicy";
-import Register from "pages/Register";
-import Test from "pages/Test";
 import React from "react";
 import {
   BrowserRouter as Router,
@@ -11,21 +6,29 @@ import {
   Switch,
 } from "react-router-dom";
 
+const Contact_Us = React.lazy(() => import("pages/Contact_Us"));
+const Landing = React.lazy(() => import("pages/Landing"));
+const PrivacyPolicy = React.lazy(() => import("pages/PrivacyPolicy"));
+const Register = React.lazy(() => import("pages/Register"));
+const Test = React.lazy(() => import("pages/Test"));
+
 function App() {
   return (
     <Router>
       <div className="App">
         <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route path="/contact-us" component={Contact_Us} />
-          <Route path="/policy-privacy" component={PrivacyPolicy} />
+          <React.Suspense fallback={<p>loading....</p>}>
+            <Route exact path="/" component={Landing} />
+            <Route path="/contact-us" component={Contact_Us} />
+            <Route path="/policy-privacy" component={PrivacyPolicy} />
 
-          {/* Test */}
-          <Route path="/register" component={Register} />
-          <Route path="/test" component={Test} />
+            {/* Test */}
+            <Route path="/register" component={Register} />
+            <Route path="/test" component={Test} />
 
-          {/* No match */}
-          <Route component={() => <Redirect to="/" />} />
+            {/* No match */}
+            <Route component={() => <Redirect to="/" />} />
+          </React.Suspense>
         </Switch>
       </div>
     </Router>
